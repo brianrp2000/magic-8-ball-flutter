b42bd64228f20d603b72f68f41b5191a60c92b20
brianrp2000 <brodr253@fiu.edu>
2019-10-09 18:19:07 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          body: Magic8Ball(),
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade700,
+          ),
+        ),
       ),
     );
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int random = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$random.png'),
+        onPressed: () {
+          setState(() {
+            random = Random().nextInt(5) + 1;
+          });
+        },
+      ),
+    );
+  }
+}
